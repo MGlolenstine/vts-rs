9616b35b399f727b4aa0654fb41dda4a9adaf5f4
MGlolenstine <mglolenstine@gmail.com>
2020-06-30 18:44:09 +0200

    Made all methods public.

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ struct VirtualTouchScreen {
 
 impl VirtualTouchScreen {
     /// Create new instance with default location at `/dev/virtual_touchscreen`
-    fn new() -> VirtualTouchScreen {
+    pub fn new() -> VirtualTouchScreen {
         VirtualTouchScreen {
             filename: "/dev/virtual_touchscreen".to_string(),
             queue: Vec::new(),
@@ -16,7 +16,7 @@ impl VirtualTouchScreen {
     }
 
     /// Create new instance with a custom location of the kernel module entry
-    fn new_with_path(path: String) -> VirtualTouchScreen {
+    pub fn new_with_path(path: String) -> VirtualTouchScreen {
         VirtualTouchScreen {
             filename: path,
             queue: Vec::new(),
@@ -24,17 +24,17 @@ impl VirtualTouchScreen {
     }
 
     /// Remove all actions from the queue
-    fn clear_queue(&mut self) {
+    pub fn clear_queue(&mut self) {
         self.queue.clear();
     }
 
     /// Remove last action from the queue
-    fn pop_queue(&mut self) {
+    pub fn pop_queue(&mut self) {
         self.queue.pop();
     }
 
     /// Execute the queue
-    fn submit_queue(&mut self) {
+    pub fn submit_queue(&mut self) {
         let mut full_command = String::new();
         for s in self.queue.clone() {
             full_command += &s;
@@ -47,23 +47,23 @@ impl VirtualTouchScreen {
     }
 
     /// Set active pointer
-    fn set_pointer(&mut self, pointer: u32) {
+    pub fn set_pointer(&mut self, pointer: u32) {
         self.queue.push(format!("s {}\na {}\n", pointer, pointer));
     }
 
     /// Set pointer position
-    fn set_position(&mut self, x: u32, y: u32) {
+    pub fn set_position(&mut self, x: u32, y: u32) {
         self.set_x(x);
         self.set_y(y);
     }
 
     /// Set X coordinate
-    fn set_x(&mut self, x: u32) {
+    pub fn set_x(&mut self, x: u32) {
         self.queue.push(format!("X {}\nx {}\n", x, x));
     }
 
     /// Set Y coordinate
-    fn set_y(&mut self, y: u32) {
+    pub fn set_y(&mut self, y: u32) {
         self.queue.push(format!("Y {}\ny {}\n", y, y));
     }
 }
